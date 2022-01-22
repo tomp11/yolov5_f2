f7db76b88f5918ef2a7e13384e997fda5fc168c6
princehomes702 <princehomes702@gmail.com>
2022-01-22 21:47:00 +0900

    init

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -44,13 +44,13 @@ class Loggers():
         self.logger = logger  # for printing results to console
         self.include = include
         self.keys = ['train/box_loss', 'train/obj_loss', 'train/cls_loss',  # train loss
-                     'metrics/precision', 'metrics/recall', 'metrics/mAP_0.5', 'metrics/mAP_0.5:0.95',  # metrics
+                     'metrics/precision', 'metrics/recall', 'metrics/mAP_0.5', 'metrics/mAP_0.5:0.95',"metrics/F2",  # metrics
                      'val/box_loss', 'val/obj_loss', 'val/cls_loss',  # val loss
                      'x/lr0', 'x/lr1', 'x/lr2']  # params
-        self.best_keys = ['best/epoch', 'best/precision', 'best/recall', 'best/mAP_0.5', 'best/mAP_0.5:0.95',]
+        self.best_keys = ['best/epoch', 'best/precision', 'best/recall', 'best/mAP_0.5', 'best/mAP_0.5:0.95',"best/F2"]
         for k in LOGGERS:
             setattr(self, k, None)  # init empty logger dictionary
-        self.csv = True  # always log to csv
+        self.csv = False  # always log to csv
 
         # Message
         if not wandb:
@@ -127,7 +127,7 @@ class Loggers():
 
         if self.wandb:
             if best_fitness == fi:
-                best_results = [epoch] + vals[3:7]
+                best_results = [epoch] + vals[3:8]
                 for i, name in enumerate(self.best_keys):
                     self.wandb.wandb_run.summary[name] = best_results[i]  # log best results in the summary
             self.wandb.log(x)
